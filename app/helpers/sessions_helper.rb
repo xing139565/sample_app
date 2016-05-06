0d9245672c4c25fbe1b9792e80fa0c865c5cf48e
邢淼 <xingmiao1990@qq.com>
2016-05-06 14:40:11 +0800

    Finish sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,10 +13,15 @@ module SessionsHelper
   def current_user=(user)
     @current_user = user
   end
-  
+
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(version: 20160505145733) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
